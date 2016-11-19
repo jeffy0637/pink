3eae6b984fb6f84e11c32285b4d59d759e62a35a
jeffy0637 <jeffy0637@gmail.com>
2016-11-19 16:14:41 +0800

    行程開始時間上傳 XML待改

--- app/src/main/java/m/mcoupledate/BoardFragment.java
+++ app/src/main/java/m/mcoupledate/BoardFragment.java
@@ -340,33 +340,34 @@ public class BoardFragment extends Fragment {
         new Firebase(url).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(final DataSnapshot dataSnapshot, String s) {
-                //這段用來動態新增list
-                final ArrayList<Pair<Long, String>> mItemArray = new ArrayList<>();
-                final int addItems = 0;
-                for (int i = 0; i < addItems; i++) {
-                    long id = sCreatedItems++;
-                    mItemArray.add(new Pair<>(id, "景點名稱"));
-                }
-                //這段用來header的景點數計算 天數計算還有問題
-                final int[] count = {0};
-                final int column = mColumns;
-                final View header = View.inflate(getActivity(), R.layout.column_header, null);
-                final ItemAdapter listAdapter = new ItemAdapter(header, mItemArray, R.layout.column_item, R.id.item_layout, true);
-                ((TextView) header.findViewById(R.id.text)).setText("第" + (mColumns + 1) + "天");
-                ((TextView) header.findViewById(R.id.item_count)).setText("景點數 : " + mItemArray.size());
-                header.setOnClickListener(new View.OnClickListener() {
-                    //這段是新增新的景點(點擊header)
-                    @Override
-                    public void onClick(View v) {
+                if((""+dataSnapshot.child("tId").getValue()).equals(tId)){
+                    //這段用來動態新增list
+                    final ArrayList<Pair<Long, String>> mItemArray = new ArrayList<>();
+                    final int addItems = 0;
+                    for (int i = 0; i < addItems; i++) {
                         long id = sCreatedItems++;
-                        Pair item = new Pair<>(id, "景點名稱");
-                        mBoardView.addItem(column, mItemArray.size(), item, true);
+                        mItemArray.add(new Pair<>(id, "景點名稱"));
+                    }
+                    //這段用來header的景點數計算 天數計算還有問題
+                    final int[] count = {0};
+                    final int column = mColumns;
+                    final View header = View.inflate(getActivity(), R.layout.column_header, null);
+                    final ItemAdapter listAdapter = new ItemAdapter(header, mItemArray, R.layout.column_item, R.id.item_layout, true);
+                    ((TextView) header.findViewById(R.id.text)).setText("第" + (mColumns + 1) + "天");
+                    ((TextView) header.findViewById(R.id.item_count)).setText("景點數 : " + mItemArray.size());
+                    header.setOnClickListener(new View.OnClickListener() {
+                        //這段是新增新的景點(點擊header)
+                        @Override
+                        public void onClick(View v) {
+                            long id = sCreatedItems++;
+                            Pair item = new Pair<>(id, "景點名稱");
+                            mBoardView.addItem(column, mItemArray.size(), item, true);
 
-                        //mBoardView.moveItem(4, 0, 0, true);
-                        //mBoardView.removeItem(column, 0);
-                        //mBoardView.moveItem(0, 0, 1, 3, false);
-                        //mBoardView.replaceItem(0, 0, item1, true);
-                        //((TextView) header.findViewById(R.id.item_count)).setText("" + mItemArray.size());
+                            //mBoardView.moveItem(4, 0, 0, true);
+                            //mBoardView.removeItem(column, 0);
+                            //mBoardView.moveItem(0, 0, 1, 3, false);
+                            //mBoardView.replaceItem(0, 0, item1, true);
+                            //((TextView) header.findViewById(R.id.item_count)).setText("" + mItemArray.size());
 
 //                        //在特定行程加入景點
 //                        if(count[0] == 0){
@@ -382,35 +383,36 @@ public class BoardFragment extends Fragment {
 //                            siteRef.setValue(site);
 //                            count[0]++;
 //                        }
-                        //在特定行程加入景點
-                        Firebase siteRef = (dataSnapshot.child("site").child("day" + (column + 1)).child("" + (mItemArray.size() - 1)).getRef());
-                        Site site = new Site(mItemArray.size() - 1, "3小地方", 8, 5);//資料更改後不準了 day拿掉變成order 所以路徑要指到哪一天才對
-                        siteRef.setValue(site);
-                        ((TextView) header.findViewById(R.id.item_count)).setText("景點數 : " + mItemArray.size());
-                    }
-                });
-                //長按header輸入要刪除的景點(應該改成整列清除)
-                header.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
-                        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),AlertDialog.BUTTON_POSITIVE, new TimePickerDialog.OnTimeSetListener()
-                        {
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+                            //在特定行程加入景點
+                            Firebase siteRef = (dataSnapshot.child("site").child("day" + (column + 1)).child("" + (mItemArray.size() - 1)).getRef());
+                            Site site = new Site(mItemArray.size() - 1, "3小地方", 8, 5);//資料更改後不準了 day拿掉變成order 所以路徑要指到哪一天才對
+                            siteRef.setValue(site);
+                            ((TextView) header.findViewById(R.id.item_count)).setText("景點數 : " + mItemArray.size());
+                        }
+                    });
+                    //長按header輸入要刪除的景點(應該改成整列清除)
+                    header.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+                            TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),AlertDialog.BUTTON_POSITIVE, new TimePickerDialog.OnTimeSetListener()
                             {
-                                ((TextView) header.findViewById(R.id.item_count)).setText("開始時間: " + hourOfDay + "時" + minute + "分");
-                                startHour = Integer.toString(hourOfDay);
-                                startMin = Integer.toString(minute);
-                                Toast.makeText(getActivity(),"改成"+startHour+":"+startMin,Toast.LENGTH_SHORT).show();
-                            }
-                        }, hourOfDay, minute, false);
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+                                {
+                                    ((TextView) header.findViewById(R.id.departureTime)).setText("開始時間: " + hourOfDay + "時" + minute + "分");
+                                    startHour = Integer.toString(hourOfDay);
+                                    startMin = Integer.toString(minute);
+                                    Toast.makeText(getActivity(),"1改成"+startHour+":"+startMin,Toast.LENGTH_SHORT).show();
+                                }
+                            }, hourOfDay, minute, false);
 
-                        timePickerDialog.show();
-                        return true;//true為結束長按動作後不再執行短按
-                    }
-                });
-                mBoardView.addColumnList(listAdapter, header, false);
-                mColumns++;
+                            timePickerDialog.show();
+                            return true;//true為結束長按動作後不再執行短按
+                        }
+                    });
+                    mBoardView.addColumnList(listAdapter, header, false);
+                    mColumns++;
+                }
             }
 
             @Override
@@ -504,7 +506,7 @@ public class BoardFragment extends Fragment {
                                     @Override
                                     public void onTimeSet(TimePicker view, int hourOfDay, int minute)
                                     {
-                                        ((TextView) header.findViewById(R.id.item_count)).setText("開始時間: " + hourOfDay + "時" + minute + "分");
+                                        ((TextView) header.findViewById(R.id.departureTime)).setText("開始時間: " + hourOfDay + "時" + minute + "分");
                                         startHour = Integer.toString(hourOfDay);
                                         startMin = Integer.toString(minute);
                                         Toast.makeText(getActivity(),"改成"+startHour+":"+startMin,Toast.LENGTH_SHORT).show();
